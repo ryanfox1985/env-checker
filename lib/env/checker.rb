038dabe44bf1e362b81a700f12c7af9fb9fcc0b3
Guillermo Guerrero <wolf.fox1985@gmail.com>
2016-11-04 15:24:16 +0100

    Initial gem.

---
+++ lib/env/checker.rb
@@ -0,0 +1,7 @@
+require "env/checker/version"
+
+module Env
+  module Checker
+    # Your code goes here...
+  end
+end
---
+++ lib/env/checker/version.rb
@@ -0,0 +1,5 @@
+module Env
+  module Checker
+    VERSION = "0.1.0"
+  end
+end
